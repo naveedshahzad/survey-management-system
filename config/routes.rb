0507d3f96b66721bc25ef22c7b7e55a991320381
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2019-05-30 00:44:12 +0500

    Fixed default root url to point to surveys index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :respondents, ActiveAdmin::Devise.config.merge(path: :respondent_portal)
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'admin/dashboard#index'
+  root to: 'admin/surveys#index'
 end
